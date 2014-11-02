1eaed0aa2dbb84516110abe421f898b9671fc0f2
Dmytro Karpovych <ZAYEC77@gmail.com>
2014-11-03 01:55:03 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-gulp-casperjs
-=============
+# gulp-casperjs
 
-gulp plugin for CasperJS
+A [gulp](https://github.com/gulpjs/gulp) plugin for runnig [CasperJS](https://github.com/n1k0/casperjs) tests
+
+## Install
+
+```
+npm install --save-dev gulp-casperjs
+```
+## Usages
+
+gulp file:
+
+```js
+var casperJs = require('gulp-casperjs');
+gulp.task('test', function () {
+  gulp.src('Globs of test files')
+    .pipe(casperJs());
+});
+
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var spawn = require('child_process').spawn;
+var PluginError = gutil.PluginError;
+
+const PLUGIN_NAME = 'gulp-casper-js';
+
+function casper(options) {
+    options = options || {};
+
+    var files = [];
+
+    var read = function(file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new PluginError({
+                plugin: PLUGIN_NAME,
+                message: 'Streams are not supported.'
+            }));
+            return cb(null, file);
+        }
+        files.push(file.path);
+
+        this.push(file);
+
+        cb(null, file);
+    };
+
+    var end = function(cb) {
+        var casperChild = spawn('casperjs', ['test'].concat(files));
+
+        casperChild.stdout.on('data', function(data) {
+            var msg = data.toString().slice(0, -1);
+            gutil.log(PLUGIN_NAME + ':', msg);
+        });
+
+        casperChild.on('close', function(code) {
+            var success = code === 0;
+            if (!success) {
+                this.emit('error', new PluginError({
+                    plugin: PLUGIN_NAME,
+                    message: 'code ' + code
+                }));
+            }
+            cb();
+        });
+    };
+
+    return through.obj(read, end);
+}
+
+module.exports = casper;
